27926dc28900c26aa4749e1ab5e0d4e3d6d64138
Ilia Maltsev <m--i--v@yandex.ru>
2022-01-05 00:05:52 +0500

    push 05.01.2022

---
+++ script.js
@@ -0,0 +1,62 @@
+const closePopupButton = document.querySelector('.popup__close-button');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const overlay = document.querySelector('.popup');
+const popupOpened = 'popup_opened'
+
+openPopupButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  overlay.classList.add(popupOpened);
+})
+
+closePopupButton.addEventListener('click', function() {
+  overlay.classList.remove(popupOpened);
+})
+/*
+overlay.addEventListener('click', function() {
+  overlay.classList.remove(popupOpened);
+})
+*/
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Escape') {
+    overlay.classList.remove(popupOpened);
+  }
+})
+
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let inputName = document.querySelector('.popup__input-field_name');
+let inputAbout = document.querySelector('.popup__input-field_about');
+const profileSave = document.querySelector('.popup__submit-button');
+
+function profileChange () {
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+}
+
+profileSave.addEventListener('click', function() {
+  profileChange();
+  overlay.classList.remove(popupOpened);
+})
+
+let likeButton = document.querySelectorAll('.element__like');
+const likeButtonActive = 'element__like_active';
+
+/*
+for (var i = 0; i < likeButton.length; i++) {
+  likeButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    likeButton.classList.toggle(likeButtonActive);
+  })
+}
+*/
+/*
+likeButton.onclick = function() {
+  likeButton.classList.toggle(likeButtonActive);
+}
+*/
+
+likeButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  likeButton.classList.toggle(likeButtonActive);
+})
+

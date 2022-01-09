21bb5777a657b197b55ae6e4be1a2e2e257e8ffd
Ilia Maltsev <m--i--v@yandex.ru>
2022-01-10 00:22:41 +0500

    1st check fixes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const closePopupButton = document.querySelector('.popup__close-button');
 const openPopupButton = document.querySelector('.profile__edit-button');
 const overlay = document.querySelector('.popup');
-const popupOpened = 'popup_opened'
+const popupOpened = 'popup_opened';
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+const form = document.querySelector('.popup__input-fields');
+let inputName = form.elements['name'];
+let inputAbout = form.elements['about'];
 
 openPopupButton.addEventListener('click', function(event) {
   event.preventDefault();
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
   overlay.classList.add(popupOpened);
 })
 
@@ -12,50 +19,13 @@ closePopupButton.addEventListener('click', function() {
   overlay.classList.remove(popupOpened);
 })
 
-document.addEventListener('keydown', function(event) {
-  if (event.code === 'Escape') {
-    overlay.classList.remove(popupOpened);
-  }
-})
-
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let inputName = document.querySelector('.popup__input-field_name');
-let inputAbout = document.querySelector('.popup__input-field_about');
-const profileSave = document.querySelector('.popup__submit-button');
-
-function profileChange () {
+function profileChange() {
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
 }
 
-profileSave.addEventListener('click', function() {
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
   profileChange();
   overlay.classList.remove(popupOpened);
-})
-
-inputName.addEventListener('keydown', function(event) {
-  if (event.code === 'Enter') {
-    profileChange();
-    overlay.classList.remove(popupOpened);
-  }
-})
-
-inputAbout.addEventListener('keydown', function(event) {
-  if (event.code === 'Enter') {
-    profileChange();
-    overlay.classList.remove(popupOpened);
-  }
-})
-
-let likeButton = document.querySelectorAll('.element__like');
-const likeButtonActive = 'element__like_active';
-let likeButtonArray = Array.prototype.slice.call(likeButton)
-
-
-for (let i = 0; i < likeButtonArray.length; ++i) {
-  likeButtonArray[i].addEventListener('click', function(event) {
-    event.preventDefault();
-    likeButtonArray[i].classList.toggle(likeButtonActive);
-  })
-}
+});

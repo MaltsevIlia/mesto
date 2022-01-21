e4fe6680396772a9b5d3587a27d79f6db00a4d16
Ilia Maltsev <m--i--v@yandex.ru>
2022-01-21 23:14:19 +0500

    switch to develop

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,31 @@
-const closePopupButton = document.querySelector('.popup__close-button');
-const openPopupButton = document.querySelector('.profile__edit-button');
-const overlay = document.querySelector('.popup');
+//глобальные постоянные
+const closePopupButton = document.querySelectorAll('.popup__close-button'); //создается псевдомассив, к нему не применяются слушатели
+const openProfilePopupButton = document.querySelector('.profile__edit-button');
+const openAddPicPopupButton = document.querySelector('.profile__add-button')
+const overlayProfile = document.querySelector('.popup__profile');
+const overlayAddPic = document.querySelector('.popup__place');
 const popupOpened = 'popup_opened';
+//переменные для разметки профиля и карточек
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+let place = document.querySelector('.element__image-title');
+let link = document.querySelector('.element__image');
+//постоянная для форм
 const form = document.querySelector('.popup__input-fields');
+// инпуты для профиля
 let inputName = form.elements['name'];
 let inputAbout = form.elements['about'];
-/*переменные для лайка*/
+// инпуты для карточек с картинками
+let inputPlace = form.elements['place'];
+let inputLink = form.elements['link'];
+//переменные для лайка
 let likeButton = document.querySelectorAll('.element__like');
 const likeButtonActive = 'element__like_active';
 let likeButtonArray = Array.prototype.slice.call(likeButton);
+//темплейт
+const templateCards = document.querySelector('#place-card').content.querySelector('.element__image-title');
 
+const cardsWrap = document.querySelector('.elements__list');
 
 const initialCards = [
   {
@@ -40,15 +54,48 @@ const initialCards = [
   }
 ];
 
-function closePopup(event) {
-  overlay.classList.remove(popupOpened);
+const renderCards = (item) => {
+  const cards = templateCards.cloneNode(true);
+  /*const cardTitle = cards.querySelector(.element__image-title);
+  const cardImage = cards.querySelector(.element__image);*/
+
 }
 
-function openPopup(event) {
+initialCards.forEach(item => {
+  renderCards(item, cardsWrap)
+})
+
+//функция добавления карточек через массив и темплейт
+
+
+
+/*
+function closeProfilePopup(event) {
+  overlayProfile.classList.remove(popupOpened);
+}
+
+function closeAddPicPopup(event) {
+  overlayAddPic.classList.remove(popupOpened);
+}
+*/
+
+closePopupButton.forEach((event) => {
+  overlayProfile.classList.remove(popupOpened);
+  overlayAddPic.classList.remove(popupOpened);
+})
+
+function openProfilePopup(event) {
   event.preventDefault();
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
-  overlay.classList.add(popupOpened);
+  overlayProfile.classList.add(popupOpened);
+}
+
+function openAddPicPopup(event) {
+  event.preventDefault();
+  /*inputPlace.value = place.textContent;*/
+  /*inputLink.value = link.textContent;*/
+  overlayAddPic.classList.add(popupOpened);
 }
 
 function profileChange(event) {
@@ -58,9 +105,13 @@ function profileChange(event) {
   closePopup();
 }
 
-openPopupButton.addEventListener('click', openPopup)
+openProfilePopupButton.addEventListener('click', openProfilePopup);
 
-closePopupButton.addEventListener('click', closePopup)
+openAddPicPopupButton.addEventListener('click', openAddPicPopup);
+
+closePopupButton.addEventListener('click', closeProfilePopup);
+
+closePopupButton.addEventListener('click', closeAddPicPopup);
 
 form.addEventListener('submit', profileChange);
 
@@ -70,4 +121,4 @@ for (let i = 0; i < likeButtonArray.length; ++i) {
     event.preventDefault();
     likeButtonArray[i].classList.toggle(likeButtonActive);
   })
-}
+};
